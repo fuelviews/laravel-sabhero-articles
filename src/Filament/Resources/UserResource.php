b4f36dd13fc44f6fcd84c56a3225cc019d0185b9
Joshua Mitchener <joshua@fuelviews.com>
2025-02-12 12:40:43 -0500

    wip

--- src/Filament/Resources/UserResource.php
+++ src/Filament/Resources/UserResource.php
@@ -146,7 +146,7 @@ class UserResource extends Resource
         return [
             'index' => ListUsers::route('/'),
             'create' => CreateUser::route('/create'),
-            //'edit' => EditUser::route('/{record}/edit'),
+            'edit' => EditUser::route('/{record}/edit'),
             'view' => ViewUser::route('/{record}'),
         ];
     }

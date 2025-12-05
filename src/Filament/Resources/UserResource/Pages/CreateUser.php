44052571570f01d205494fd3716d22272f7fe278
Joshua Mitchener <64335706+thejmitchener@users.noreply.github.com>
2025-12-05 15:17:56 -0500

    Add feature_image column to pages table and enhance user password handling by adding password confirmation and hashing; improve slug generation and author status assignment in HasArticle trait. (#49)
    * Add feature_image column to pages table and enhance user password handling by adding password confirmation and hashing; improve slug generation and author status assignment in HasArticle trait.
    
    * Fix styling
    
    * Remove ExampleTest file to clean up unused test case

--- src/Filament/Resources/UserResource/Pages/CreateUser.php
+++ src/Filament/Resources/UserResource/Pages/CreateUser.php
@@ -4,19 +4,8 @@ namespace Fuelviews\SabHeroArticles\Filament\Resources\UserResource\Pages;
 
 use Filament\Resources\Pages\CreateRecord;
 use Fuelviews\SabHeroArticles\Filament\Resources\UserResource;
-use Illuminate\Support\Facades\Hash;
 
 class CreateUser extends CreateRecord
 {
     protected static string $resource = UserResource::class;
-
-    protected function mutateFormDataBeforeCreate(array $data): array
-    {
-        // Generate a random password if none provided
-        if (empty($data['password'])) {
-            $data['password'] = Hash::make(str()->random(12));
-        }
-
-        return $data;
-    }
 }
